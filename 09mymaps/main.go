1a7c5b174f5c4f93aba14f8a00c84a827b6a75f5
Noor Mohammed <mail@noormohammed.tech>
2021-09-22 12:03:49 +0530

    delete map item
    for loop for onley values

--- 09mymaps/main.go
+++ 09mymaps/main.go
@@ -9,13 +9,17 @@ func main() {
 	proLang["py"] = "Python"
 	proLang["cpp"] = "C Pluse Pluse"
 
-	fmt.Println(proLang)
 	fmt.Printf("type of proLang: %T\n", proLang)
+	fmt.Println(proLang)
 
-	var tKey string = "js"
+	var tKey, dKey string = "js", "cpp"
+
+	delete(proLang, dKey)
+	fmt.Println(proLang)
 
 	fmt.Println("targeting a key : ", proLang[tKey])
 
+	// for _, value := range proLang {  // for only iterate values
 	for key, value := range proLang {
 		fmt.Println(key, value)
 	}
