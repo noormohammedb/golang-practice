926505bacb2edf35a5c74a7bc74e1db911505f95
Noor Mohammed <mail@noormohammed.tech>
2021-09-25 19:35:55 +0530

    loops

---
+++ 14loops/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	days := []string{"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Staurday"}
+
+	fmt.Println(days)
+
+	// normal forLoop
+	for d := 0; d < len(days); d++ {
+		fmt.Println(days[d])
+	}
+
+	// range forLoop
+	for i := range days {
+		fmt.Println(days[i])
+	}
+
+	// like forEach loop
+	for index, day := range days {
+		fmt.Printf("%v. %v\n", index, day)
+	}
+
+	dumpValue := 1
+
+	// like whileLook
+	for dumpValue <= 10 {
+		fmt.Print(dumpValue, ", ")
+		dumpValue++
+	}
+}
