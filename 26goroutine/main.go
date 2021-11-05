a1b4a9923a275c1e708c17c1a9d6f8b55db37894
Noor Mohammed <mail@noormohammed.tech>
2021-11-05 18:49:53 +0530

    Concurrent Go http request
    with wait group

--- 26goroutine/main.go
+++ 26goroutine/main.go
@@ -2,17 +2,47 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"net/http"
+	"sync"
 )
 
+var wg sync.WaitGroup
+
 func main() {
-	go greeter("Hello")
-	greeter("World!")
+	// go greeter("Hello")
+	// greeter("World!")
+	websiteList := []string{
+		"https://go.dev/",
+		"https://lco.dev/",
+		"https://google.com/",
+		"https://github.com/",
+		"https://gmail.com/",
+	}
+	for _, webUri := range websiteList {
+		// getStatusCode(webUri)
+		go getStatusCode(webUri)
+		wg.Add(1)
+	}
+
+	wg.Wait()
 }
 
-func greeter(argData string) {
-	for i := 0; i < 6; i++ {
-		time.Sleep(300 * time.Millisecond)
-		fmt.Println(argData)
+// func greeter(argData string) {
+// 	for i := 0; i < 6; i++ {
+// 		time.Sleep(300 * time.Millisecond)
+// 		fmt.Println(argData)
+// 	}
+// }
+
+func getStatusCode(uriData string) {
+	defer wg.Done()
+
+	res, err := http.Get(uriData)
+
+	if err != nil {
+		fmt.Println("Error in api call ", uriData)
 	}
+
+	fmt.Printf("%d %s\n", res.StatusCode, uriData)
+
 }
